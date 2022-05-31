073b6a9406a3091d86c269c88944c7d5f64e6dd3
Will Sims <willsims@Wills-MacBook-Pro.local>
2022-05-30 21:32:10 -0500

    sf

--- original.py
+++ original.py
@@ -16,29 +16,29 @@
 def main():
     """ <String describing the main() function here> """
 
-    #Initialize Variables (newpaperCost = $1, commissionRate = 25%)
+    # Initialize Variables
     NEWSPAPER_COST = 1.00
     COMMISSION_RATE = 0.25
 
     # Prompt user for name
     name = input("Hello, what is your name?\t")
 
-    #Display welcome message
+    # Display welcome message
     print("\nHello " + name + ", this program will assist you in calculating your weekly pay as a paper carrier.")
     print("To get started we will first need some information from you:")
 
-    #Prompt user for number of papers on route
+    # Prompt user for number of papers on route
     num_papers_on_route = int(input("\nHow many papers do you delivere on your route?\t"))
-    #Prompt user for number of days paper is delivered per week
+    # Prompt user for number of days paper is delivered per week
     num_days_per_week = int(input("How many days a week do you deliver papers?\t"))
-    #Prompt user for amount of tips received z week
+    # Prompt user for amount of tips received z week
     num_tips_per_week = int(input("How much did you receive in tips this week?\t"))
 
     total_papers_per_week = num_papers_on_route * num_days_per_week
     weekly_salary = total_papers_per_week * NEWSPAPER_COST * COMMISSION_RATE
     total_pay = weekly_salary + num_tips_per_week
 
-    #Display the results
+    # Display the results
     print("\nThe total number of papers you delivered for the week was " + str(total_papers_per_week) + " papers.")
     print("Using a predetermined newspaper cost of $1 and a commission rate of 25%, your weekly salary is $" + str(weekly_salary))
     print("The total amount you collected in tips was $" + str(num_tips_per_week))
